347965db1b540004293f33ccc7eaafe9d4c81bb1
aikozindev <aikozindev@gmail.com>
2023-04-04 20:43:55 +0300

    Исправил тесты по подключению к сессии телефона (infinity = true) Исправил расчеты времени - теперь сравнивается не с точностью до секунды, а +- 5 секунд

--- src/main/java/org/example/filebox/helpers/LoggerHelper.java
+++ src/main/java/org/example/filebox/helpers/LoggerHelper.java
@@ -19,6 +19,16 @@ public class LoggerHelper {
         Assertions.assertEquals(value1, value2);
     }
 
+    public static <T> void isEqualsTime(String message, T value1, T value2) {
+        Logger logger = create(localeClassName);
+        long timeOffset = Math.abs((long) value1 - (long) value2);
+        if (timeOffset <= 2) {
+            value1 = value2;
+        }
+        logger.info(String.format("%s: %s == %s", message, value1, value2));
+        Assertions.assertEquals(value1, value2);
+    }
+
     public static void isTrue(String message, boolean value) {
         Logger logger = create(localeClassName);
         logger.info(String.format("%s: %s", message, value));
